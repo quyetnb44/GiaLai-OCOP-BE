c7b40bca792361a6bbf0567917bc0c26abb4cd6f
quyetnb44 <nguyenbaquyet9a4cpr@gmail.com>
2025-10-07 14:01:29 +0700

    test

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GiaLaiOCOP.Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("Backend đang chạy, nhưng đây là API, không phải trang web.");
+        }
+    }
+}
